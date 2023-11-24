e3c43b095e44329e6d098d38138efdcd37372c00
Gan Tu <github@tugan.dev>
2023-11-23 19:22:15 -0500

    add some project details

---
+++ src/pages/projects/project-details.jsx
@@ -0,0 +1,142 @@
+export const projects = [
+  {
+    name: 'GoList',
+    description:
+      'An URL shortening service that lets you quickly browse a collection of links or files with one short URL.',
+    link: {
+      href: 'https://goli.st',
+      label: 'goli.st',
+    },
+    logo: 'https://goli.st/logo192.png',
+  },
+  {
+    name: 'Project Music',
+    description:
+      'An explorative UI concept website for being a hub for artists to collaborate and showcase their work and engage with their fans.',
+    link: {
+      href: 'https://project-music.tugan.app',
+      label: 'project-music',
+    },
+    logo: 'https://i.pinimg.com/originals/a1/ba/f0/a1baf0aa72ec15ae66a72c0d316d98cc.jpg',
+  },
+  {
+    name: 'AirBnB Website Clone',
+    description:
+      'A simple UI Clone of AirBnB website built using Tailwind CSS. AirBnB is an online marketplace for homestays and experiences.',
+    link: {
+      href: 'https://airbnb.tugan.app',
+      label: 'tugan.app',
+    },
+    logo: 'https://play-lh.googleusercontent.com/1zfN_BL13q20v0wvBzMWiZ_sL_t4KcCJBeAMRpOZeT3p34quM-4-pO-VcLj8PJNXPA0',
+  },
+  {
+    name: 'Google File System',
+    description:
+      'A distributed Google File System (GFS), partially implemented in C++.',
+    link: {
+      href: 'https://github.com/Gan-Tu/cppGFS2.0',
+      label: 'github.com',
+    },
+    logo: 'https://static.vecteezy.com/system/resources/thumbnails/017/396/811/small_2x/files-by-google-free-png.png',
+  },
+  {
+    name: 'Interactive Stonehenge',
+    description:
+      'An interactive fun simulation of Stonehenge in browser built in Three.js. Users can destroy, restore, add, remove, and customize objects and animations for creative fun.',
+    link: {
+      href: 'https://gan-tu.github.io/StonehengeJS',
+      label: 'github.com',
+    },
+    logo: 'https://cdn-icons-png.flaticon.com/512/533/533494.png',
+  },
+  {
+    name: 'Cloth Simulator',
+    description:
+      'A mass-spring system simulator that animates realistic hanging, pinned, falling, colliding, and folding cloth behaviors.',
+    link: {
+      href: 'https://github.com/Gan-Tu/Cloth-Simulator',
+      label: 'github.com',
+    },
+    logo: 'https://images.vexels.com/media/users/3/239390/isolated/preview/c7e3cbe7119e7a2af3b5d07c60ff095d-hanging-towel-cut-out.png',
+  },
+  {
+    name: 'Graphic Renderer',
+    description:
+      'A fully functional, accelerated physically-based renderer using the pathtracing algorithm.',
+    link: {
+      href: 'https://github.com/Gan-Tu/Graphic-Renderer',
+      label: 'github.com',
+    },
+    logo: 'https://cdn-icons-png.flaticon.com/512/14/14790.png',
+  },
+  {
+    name: 'Netflix iOS Clone',
+    description:
+      'A simple UI Clone of Netflix built using SwiftUI. Netflix is an American subscription video on-demand over-the-top streaming service.',
+    link: {
+      href: 'https://github.com/Gan-Tu/NetflixClone',
+      label: 'github.com',
+    },
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/2048px-Netflix_icon.svg.png',
+  },
+  {
+    name: 'Tinder iOS Clone',
+    description:
+      'A simple UI Clone of Tinder built using SwiftUI. Tinder is an online dating and geosocial networking iOS application.',
+    link: {
+      href: 'https://github.com/Gan-Tu/iOS-TinderClone',
+      label: 'github.com',
+    },
+    logo: 'https://cdn-icons-png.flaticon.com/512/2504/2504943.png',
+  },
+  {
+    name: 'Twitter Website Clone',
+    description:
+      'A simple UI Clone of Twitter website built using Tailwind CSS. Twitter is social networking service.',
+    link: {
+      href: 'https://twitter.tugan.app',
+      label: 'tugan.app',
+    },
+    logo: 'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/twitter-app-icon.png',
+  },
+  {
+    name: 'Spotify Website Clone',
+    description:
+      'A simple UI Clone of Spotify website built using Tailwind CSS. Spotify is a digital music, podcast, and video streaming service.',
+    link: {
+      href: 'https://spotify.tugan.app',
+      label: 'tugan.app',
+    },
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Spotify_App_Logo.svg/2048px-Spotify_App_Logo.svg.png',
+  },
+  {
+    name: 'Slot Machine iOS Game',
+    description:
+      'A simple but entertaining slot machine iOS game built using SwiftUI.',
+    link: {
+      href: 'https://github.com/Gan-Tu/iOS-SlotMachine2023',
+      label: 'github.com',
+    },
+    logo: 'https://www.brandonschitka.ca/img/Slots.png',
+  },
+  {
+    name: 'ChocoPyCompiler',
+    description:
+      'A compiler for ChocoPy, a statically typed dialect of Python.',
+    link: {
+      href: 'https://github.com/Gan-Tu/ChocoPyCompiler',
+      label: 'github.com',
+    },
+    logo: 'https://cdn.icon-icons.com/icons2/1381/PNG/512/python_94570.png',
+  },
+  {
+    name: 'Mesh Editor',
+    description:
+      'A mesh editor that renders and edits basic COLLADA mesh files.',
+    link: {
+      href: 'https://github.com/Gan-Tu/Mesh-Editor',
+      label: 'github.com',
+    },
+    logo: 'https://cdn-icons-png.flaticon.com/512/174/174013.png',
+  },
+]
