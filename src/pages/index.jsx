baed51f4ac524e156ca5b193d8331db14674f59f
Gan Tu <github@tugan.dev>
2023-11-23 19:35:25 -0500

    update meta tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -309,7 +309,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Gan, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Gan, a software engineer and aspirational entrepreneur based in New York City."
         />
       </Head>
       <Container className="mt-9">
