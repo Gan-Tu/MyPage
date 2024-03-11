862ff56beb7786a850140587d9af434199c35d7d
Gan Tu <github@tugan.dev>
2024-03-11 10:45:54 -0400

    upgrade to next 13 with analytics

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    newNextLinkBehavior: true,
+    // newNextLinkBehavior: true,
     scrollRestoration: true,
   },
 }
