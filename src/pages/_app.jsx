6c4cfde32a6e9f2ca3a1073cbb3580b6c0c7d6bc
Gan Tu <dev@tugan.io>
2026-01-01 02:06:28 -0800

    remove intercom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import Intercom from '@intercom/messenger-js-sdk'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { useEffect, useRef } from 'react'
@@ -21,10 +20,6 @@ function usePrevious(value) {
 
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
-  Intercom({
-    app_id: 'od46lrxi',
-  })
-
   return (
     <>
       <div className="fixed inset-0 flex justify-center sm:px-8">
